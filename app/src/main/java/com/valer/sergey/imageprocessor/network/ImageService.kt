29eaf5290620504de976f8ed171a7f3263c04c45
sergey.valyer <sergey.valyer@spl.co>
2019-05-12 22:10:03 +0300

    moved network service

--- app/src/main/java/com/valer/sergey/imageprocessor/network/ImageService.kt
+++ app/src/main/java/com/valer/sergey/imageprocessor/network/ImageService.kt
@@ -2,6 +2,8 @@ package com.valer.sergey.imageprocessor.network
 
 import io.reactivex.Single
 import okhttp3.ResponseBody
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.http.GET
 import retrofit2.http.Url
 
@@ -9,5 +11,15 @@ interface ImageService {
 
     @GET
     fun loadImage(@Url url: String): Single<ResponseBody>
+
+    companion object {
+        val instance: ImageService by lazy {
+            val retrofit = Retrofit.Builder()
+                    .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                    .baseUrl("https://www.test.ru")
+                    .build()
+            retrofit.create(ImageService::class.java)
+        }
+    }
 
 }
--- app/src/main/java/com/valer/sergey/imageprocessor/repository/ImageProcessingRepositoryImpl.kt
+++ app/src/main/java/com/valer/sergey/imageprocessor/repository/ImageProcessingRepositoryImpl.kt
@@ -3,19 +3,13 @@ package com.valer.sergey.imageprocessor.repository
 import com.valer.sergey.imageprocessor.network.ImageService
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
-import retrofit2.Retrofit
-import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 
 class ImageProcessingRepositoryImpl : ImageProcessingRepository {
 
     private val imageService: ImageService
 
     init {
-        val retrofit = Retrofit.Builder()
-                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-                .baseUrl("https://www.test.ru")
-                .build()
-        imageService = retrofit.create(ImageService::class.java)
+        imageService = ImageService.instance
     }
 
     override fun loadImage(address: String) =
